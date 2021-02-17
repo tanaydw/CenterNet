a6ccbd44e6b8fb60f3bc5baf5283ca7e370ac1bb
Tanay <tanayd.97@gmail.com>
2021-02-17 13:23:20 +0530

    added video demo

--- src/demo.py
+++ src/demo.py
@@ -26,15 +26,29 @@ def demo(opt):
   if opt.demo == 'webcam' or \
     opt.demo[opt.demo.rfind('.') + 1:].lower() in video_ext:
     cam = cv2.VideoCapture(0 if opt.demo == 'webcam' else opt.demo)
+    out_name = (opt.demo.split('/')[-1]).split('.')[0]
     detector.pause = False
+    cnt = 0
     while True:
         _, img = cam.read()
+        if img is None:
+            try:
+                out.release()
+            except:
+                print('File not found!!!')
+            return
+        cnt += 1
         # cv2.imshow('input', img)
+        if cnt == 1:
+            fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+            out = cv2.VideoWriter('./results/{}_output.avi'.format(out_name), 
+                    fourcc, 10, (img.shape[1], img.shape[0]))
         ret = detector.run(img)
         time_str = ''
         for stat in time_stats:
           time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
-        print(time_str)
+        print('Frame ' + str(cnt) + ' |' + time_str)
+        out.write(ret['add_pred'])
         if cv2.waitKey(1) == 27:
             return  # esc to quit
   else:
@@ -50,12 +64,13 @@ def demo(opt):
     
     for (image_name) in image_names:
       ret = detector.run(image_name)
+      img_name = (image_name.split('/')[-1]).split('.')[0]
       time_str = ''
       for stat in time_stats:
         time_str = time_str + '{} {:.3f}s |'.format(stat, ret[stat])
       print(time_str)
-      cv2.imwrite('./results' + '/{}_add_pred.png'.format(image_name), ret['add_pred'])
-      cv2.imwrite('./results' + '/{}_bird_pred.png'.format(image_name), ret['bird_pred'])
+      cv2.imwrite('./results' + '/{}_add_pred.png'.format(img_name), ret['add_pred'])
+      cv2.imwrite('./results' + '/{}_bird_pred.png'.format(img_name), ret['bird_pred'])
 
 if __name__ == '__main__':
   opt = opts().init()
